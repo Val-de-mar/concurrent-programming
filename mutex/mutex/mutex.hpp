59a5b92c5d736b7d4cb789a1b13c4df7f71ee318
Vladimir Melnikov <>
2022-02-06 23:46:24 +0300

    Bump task mutex/mutex

---
+++ mutex/mutex/mutex.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <twist/stdlike/atomic.hpp>
+
+#include <cstdlib>
+
+namespace stdlike {
+
+class Mutex {
+ public:
+  void Lock() {
+    // Your code goes here
+  }
+
+  void Unlock() {
+    // Your code goes here
+  }
+
+ private:
+  // ???
+};
+
+}  // namespace stdlike
