251e0c589637a5934ef0e6cbc55a1b73765b3215
Vladimir Melnikov <>
2022-02-13 21:21:54 +0300

    Bump task mutex/mutex

--- mutex/mutex/mutex.hpp
+++ mutex/mutex/mutex.hpp
@@ -3,21 +3,30 @@
 #include <twist/stdlike/atomic.hpp>
 
 #include <cstdlib>
+#include <cstdint>
 
 namespace stdlike {
 
 class Mutex {
  public:
   void Lock() {
-    // Your code goes here
+    uint32_t ticket = last_waiting_.fetch_add(1);
+    while (ticket != free_ticket_.load()) {
+      swapper_.FutexWait(0);
+    }
+    swapper_.store(0);
   }
 
   void Unlock() {
-    // Your code goes here
+    free_ticket_.fetch_add(1);
+    swapper_.store(1);
+    swapper_.FutexWakeAll();
   }
 
  private:
-  // ???
+  twist::stdlike::atomic<uint32_t> last_waiting_{0};
+  twist::stdlike::atomic<uint32_t> free_ticket_{0};
+  twist::stdlike::atomic<uint32_t> swapper_{0};
 };
 
 }  // namespace stdlike
