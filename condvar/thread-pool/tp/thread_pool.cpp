fbfc8657c759637d841c2919f867f02c4d531100
Vladimir Melnikov <>
2022-03-07 14:17:22 +0300

    Bump task condvar/thread-pool

--- condvar/thread-pool/tp/thread_pool.cpp
+++ condvar/thread-pool/tp/thread_pool.cpp
@@ -6,32 +6,77 @@ namespace tp {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-static twist::util::ThreadLocalPtr<ThreadPool> pool;
+static twist::util::ThreadLocalPtr<ThreadPool> this_pool;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-ThreadPool::ThreadPool(size_t /*workers*/) {
-  // Not implemented
+ThreadPool::ThreadPool(size_t workers) {
+  workers_.reserve(workers);
+  for (size_t i = 0; i < workers; ++i) {
+    workers_.emplace_back([pool = this]() {
+      tp::this_pool = pool;
+      while (true) {
+        auto task = pool->tasks_.Take();
+        if (task) {
+          try {
+            task.value()();
+          } catch (...) {
+          }
+          --(pool->waiter_);
+        } else {
+          return;
+        }
+      }
+      tp::this_pool = nullptr;
+    });
+  }
 }
 
 ThreadPool::~ThreadPool() {
-  // Not implemented
+  assert(is_stopped_.load());
 }
 
-void ThreadPool::Submit(Task /*task*/) {
-  // Not implemented
+void ThreadPool::Submit(Task task) {
+  ++waiter_;
+  if (!tasks_.Put(std::move(task))) {
+    --waiter_;
+  }
 }
 
-void ThreadPool::Wait() {
-  // Not implemented
+void ThreadPool::WaitIdle() {
+  waiter_.Wait();
 }
 
 void ThreadPool::Stop() {
-  // Not implemented
+  tasks_.Cancel();
+  is_stopped_.store(true);
+  for (auto& worker : workers_) {
+    worker.join();
+  }
 }
 
 ThreadPool* ThreadPool::Current() {
-  return pool;
+  return this_pool;
 }
 
+ThreadPool::NullWaiter& ThreadPool::NullWaiter::operator--() {
+  std::unique_lock lock(mutex_);
+  assert(counter_ != 0);
+  --counter_;
+  if (counter_ == 0) {
+    is_null_.notify_all();
+  }
+  return *this;
+}
+ThreadPool::NullWaiter& ThreadPool::NullWaiter::operator++() {
+  std::unique_lock lock(mutex_);
+  ++counter_;
+  return *this;
+}
+void ThreadPool::NullWaiter::Wait() {
+  std::unique_lock lock(mutex_);
+  if (counter_ != 0) {
+    is_null_.wait(lock);
+  }
+}
 }  // namespace tp
--- condvar/thread-pool/tp/thread_pool.hpp
+++ condvar/thread-pool/tp/thread_pool.hpp
@@ -4,6 +4,8 @@
 #include <tp/task.hpp>
 
 #include <twist/stdlike/thread.hpp>
+#include <twist/stdlike/atomic.hpp>
+#include <twist/stdlike/atomic.hpp>
 #include <twist/util/thread_local.hpp>
 
 namespace tp {
@@ -11,6 +13,18 @@ namespace tp {
 // Fixed-size pool of worker threads
 
 class ThreadPool {
+  class NullWaiter {
+   public:
+    NullWaiter& operator--();
+    NullWaiter& operator++();
+    void Wait();
+
+   private:
+    uint32_t counter_ = 0;
+    twist::stdlike::mutex mutex_;
+    twist::stdlike::condition_variable is_null_;
+  };
+
  public:
   explicit ThreadPool(size_t workers);
   ~ThreadPool();
@@ -23,20 +37,20 @@ class ThreadPool {
   void Submit(Task task);
 
   // Waits until outstanding work count has reached zero
-  void Wait();
+  void WaitIdle();
 
   // Stops the worker threads as soon as possible
   // Pending tasks will be discarded
   void Stop();
 
   // Locates current thread pool from worker thread
-  static ThreadPool* Current() {
-    return ThreadPool::working_pool;
-  }
+  static ThreadPool* Current();
 
  private:
-  static twist::util::ThreadLocalPtr<ThreadPool> working_pool;
-  // Worker threads, task queue, etc
+  NullWaiter waiter_;
+  std::vector<twist::stdlike::thread> workers_;
+  UnboundedBlockingQueue<Task> tasks_;
+  twist::stdlike::atomic<bool> is_stopped_{false};
 };
 
 inline ThreadPool* Current() {
